1c30201b6622590628cff5a8aef76fa31ed79b1f
Kevin Perez <kevinperez478@gmail.com>
2024-01-23 16:18:14 -0800

    setting up bare bones & src/test

---
+++ test/compiler.test.js
@@ -0,0 +1,7 @@
+import assert from "node:assert/strict";
+
+describe("Compiler Test", () => {
+  it("should assert that 1 === 1", () => {
+    assert.equal(1, 1);
+  });
+});
---
+++ test/sample.test.js
@@ -0,0 +1,7 @@
+import assert from "node:assert/strict";
+
+describe("Sample Test", () => {
+  it("should test that true === true", () => {
+    assert.equal(true, true);
+  });
+});
