36a81768fb2f1d247787da19588a17cc64e269ff
Kevin Perez <kevinperez478@gmail.com>
2024-03-20 15:17:12 -0700

    working on homework 3

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,3 +1,98 @@
-export default function analyze() {
-  throw new Error("Not yet implemented")
+import * as core from "./core.js"
+
+export default function analyze(match) {
+  const analyzer = match.matcher.grammar.createSemantics().addOperation("rep", {
+    Program(statements) {
+      return new core.Program(statements.children.map((s) => s.rep()))
+    },
+    FuncDecl(_item, id, params, type, block) {},
+    ClassDecl(_order, id, _open, constructor, fundecl, _close) {},
+    Constructor(_hopper, params, initconst) {},
+    InitConst(_shot, id, exp) {},
+    ObjDecl(id, id, _new, id) {},
+    Params(paramList) {},
+    Param(id, _colon, type) {},
+    Block(_open, stmt, _close) {
+      return statements.children.map((s) => s.rep())
+    },
+    Type_array(_left, baseType, _right) {
+      return core.arrayType(baseType.rep())
+    },
+    Type_dictionary() {},
+    Type_optional(baseType, _questionMark) {
+      return core.optionalType(baseType.rep())
+    },
+    Type_function(_left, types, _right, _arrow, type) {
+      const paramTypes = types.asIteration().children.map((t) => t.rep())
+      const returnType = type.rep()
+      return core.functionType(paramTypes, returnType)
+    },
+    Type_id(id) {
+      const entity = context.lookup(id.sourceString)
+      mustHaveBeenFound(entity, id.sourceString, { at: id })
+      mustBeAType(entity, { at: id })
+      return entity
+    },
+    Type_none() {},
+    Assignment(id, _eq, experssion) {
+      return new core.Assignment(id.sourceString, experssion.rep())
+    },
+    VarDecl(modifier, id, _eq, exp) {},
+    IfStmt_with_else(_brew, exp, block, _pull, block) {},
+    IfStmt_nested_if(_else, stmt) {},
+    IfStmt_plain_if(_else, block) {},
+    ForStmt() {},
+    WhileStmt(_while, exp, block) {
+      return new core.WhileStatement(exp.rep(), block.rep())
+    },
+    CallStmt() {},
+    Call() {},
+    Args() {},
+    BreakStmt(_break) {
+      return new core.BreakStatement()
+    },
+    PrintStmt(_print, exp) {
+      return new core.PrintStatement(exp.rep())
+    },
+    ReturnStmt() {},
+    DotCall() {},
+    Exp_unary() {},
+    Exp0_or() {},
+    Exp1_and() {},
+    Exp2_relop() {},
+    Exp3_addsub() {},
+    Term_muldivmod() {},
+    Factor_power() {},
+    Primary_params() {},
+    Primary_array() {},
+    Primary_subscript() {},
+    Primary_number() {},
+    // Exp_comparison(left, op, right) {
+    //   return new core.BinaryExpression(op.sourceString, left.rep(), right.rep())
+    // },
+    // Exp1_binary(left, op, right) {
+    //   return new core.BinaryExpression(op.sourceString, left.rep(), right.rep())
+    // },
+    // Term_binary(left, op, right) {
+    //   return new core.BinaryExpression(op.sourceString, left.rep(), right.rep())
+    // },
+    // Factor_binary(left, op, right) {
+    //   return new core.BinaryExpression(op.sourceString, left.rep(), right.rep())
+    // },
+    // Factor_negation(op, operand) {
+    //   return new core.UnaryExpression(op.sourceString, operand.rep())
+    // },
+    // Primary_call(id, _open, args, _close) {},
+    // Primary_parens(_open, exp, _close) {
+    //   return new exp.rep()
+    // },
+    // Primary_id(id) {
+    //   return new core.Variable(id.sourceString)
+    // },
+    // numeral(_main, _dot, _fract, _e, _sign, _exp) {
+    //   return Number(this.sourceString)
+    // },
+  })
+
+  return analyzer(match).rep()
 }
---
+++ src/core.js
@@ -0,0 +1,143 @@
+export class Program {
+  constructor(statements, declaration) {
+    Object.assign(this, { statements, declaration })
+  }
+}
+
+export class Type {
+  static BOOLEAN = new Type("boolean")
+  static INT = new Type("pump")
+  static FLOAT = new Type("affogato")
+  static STRING = new Type("roast")
+  static NONE = new Type("none")
+  static ANY = new Type("any")
+  constructor(description) {
+    Object.assign(this, { description })
+  }
+}
+
+export class FunctionDeclaration {
+  // Example: function f(x: [int?], y: string): Vector {}
+  constructor(fun, params, body) {
+    Object.assign(this, { fun, params, body })
+  }
+}
+
+export class Function {
+  // Generated when processing a function declaration
+  constructor(name, type) {
+    Object.assign(this, { name, type })
+  }
+}
+
+// IS THIS CORRECT?
+export class FunctionType {
+  constructor(paramTypes, returnType) {
+    Object.assign(this, { paramTypes, returnType })
+  }
+}
+
+export class ClassDeclaration {
+  constructor(id, constructor, fun) {
+    Object.assign(this, { id, constructor, fun })
+  }
+}
+
+export class Constructor {
+  constructor(params, body) {
+    Object.assign(this, { params, body })
+  }
+}
+
+export class InitializeConstructor {
+  constructor(variable, initializer) {
+    Object.assign(this, { variable, initializer })
+  }
+}
+
+export class ObjectDeclaration {
+  // !!!!! COMPLETE THIS !!!!!
+}
+
+export class Assignment {
+  constructor(target, source) {
+    Object.assign(this, { target, source })
+  }
+}
+
+export class VariableDeclaration {
+  constructor(variable, initializer) {
+    Object.assign(this, { variable, initializer })
+  }
+}
+
+export class Variable {
+  constructor(name) {
+    Object.assign(this, { name })
+  }
+}
+
+export class Increment {
+  constructor(variable) {
+    this.variable = variable
+  }
+}
+
+export class Decrement {
+  constructor(variable) {
+    this.variable = variable
+  }
+}
+export class Conditional {
+  constructor(test, consequent, alternate, type) {
+    Object.assign(this, { test, consequent, alternate, type })
+  }
+}
+
+export class IfStatement {
+  constructor(test, consequent, alternate) {
+    Object.assign(this, { test, consequent, alternate })
+  }
+}
+
+export class ForStatement {
+  constructor(iterator, collection, body) {
+    Object.assign(this, { iterator, collection, body })
+  }
+}
+
+export class WhileStatement {
+  constructor(test, body) {
+    Object.assign(this, { test, body })
+  }
+}
+
+export class CallStatement {
+  // !!!!! COMPLETE THIS !!!!!
+}
+
+export class BreakStatement {}
+
+export class PrintStatement {
+  constructor(argument) {
+    this.argument = argument
+  }
+}
+
+export class ReturnStatement {
+  constructor(expression) {
+    this.expression = expression
+  }
+}
+
+export class BinaryExpression {
+  constructor(op, left, right) {
+    Object.assign(this, { op, left, right })
+  }
+}
+
+export class UnaryExpression {
+  constructor(op, operand) {
+    Object.assign(this, { op, operand })
+  }
+}
